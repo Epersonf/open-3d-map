fe22dd1fb1108570743352e710e96f3d244fc291
Eperson Mayrink <epersoncmxf@gmail.com>
2026-01-02 13:55:04 -0300

    import in progress

--- lib/core/utils/model_import.dart
+++ lib/core/utils/model_import.dart
@@ -1,14 +1,21 @@
+import 'dart:io';
+
+import 'dart:typed_data';
 import 'package:flutter_scene/scene.dart';
+import 'package:flutter_scene_importer/offline_import.dart';
+import 'package:flutter_scene_importer/importer.dart';
 
 class ModelImport {
   static Map<String, Node> _modelCache = Map<String, Node>();
 
-  static Future<Node> loadModel(String path) async {
-    if (_modelCache.containsKey(path)) {
-      return _modelCache[path]!.clone();
+  static Future<Node> loadModel(String absolutePath) async {
+    if (_modelCache.containsKey(absolutePath)) {
+      return _modelCache[absolutePath]!.clone();
     }
-    Node node = await Node.fromAsset(path);
-    _modelCache[path] = node;
+    final bytes = await File(absolutePath).readAsBytes();
+    final byteData = ByteData.sublistView(bytes);
+    final node = await Node.fromFlatbuffer(byteData);
+    _modelCache[absolutePath] = node;
     return node.clone();
   }
 
--- lib/presentation/components/viewport/viewport.dart
+++ lib/presentation/components/viewport/viewport.dart
@@ -1,9 +1,12 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Matrix4;
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' hide Matrix4;
 import 'package:open_3d_mapper/presentation/components/viewport/fly_camera_controller.dart';
 import 'package:vector_math/vector_math.dart';
 import 'package:flutter_scene/scene.dart';
+import 'package:open_3d_mapper/stores/project_store.dart';
+import 'package:open_3d_mapper/core/utils/model_import.dart';
+import 'package:open_3d_mapper/domain/asset/asset.dart';
 
 class Viewport3D extends StatefulWidget {
   const Viewport3D({super.key});
@@ -17,7 +20,11 @@ class _Viewport3DState extends State<Viewport3D>
   late PerspectiveCamera camera;
   late FlyCameraController controller;
   late final Ticker _ticker;
+
   final Scene scene = Scene();
+  final List<Node> _spawned = [];
+
+  bool ready = false;
 
   @override
   void initState() {
@@ -30,42 +37,86 @@ class _Viewport3DState extends State<Viewport3D>
 
     controller = FlyCameraController(camera);
 
-
-    // TODO: INSTANTIATE SCENE CONTENT HERE
-    final cube = Mesh(
-      CuboidGeometry(Vector3(1, 1, 1)),
-      PhysicallyBasedMaterial(environment: scene.environment)
-        ..baseColorFactor = Vector4(0.2, 0.7, 1, 1),
-    );
-
-    scene.add(Node(name: 'cube', mesh: cube));
-
-    Scene.initializeStaticResources().then((_) {
-      if (mounted) setState(() {});
+    Scene.initializeStaticResources().then((_) async {
+      await _loadProjectScene();
+      if (mounted) setState(() => ready = true);
     });
 
     _ticker = createTicker((_) {
       controller.update();
-      setState(() {});
+      if (ready && mounted) setState(() {});
     })..start();
+  }
+
+  Future<void> _loadProjectScene() async {
+    for (final n in _spawned) {
+      scene.remove(n);
+    }
+    _spawned.clear();
+
+    final project = ProjectStore.instance.project;
+    if (project == null || project.scenes.isEmpty) return;
+
+    final root = project.scenes.first.rootObjects;
+
+    for (final go in root) {
+      if (go.assetId == null) continue;
+
+      final asset = project.assets.firstWhere(
+        (a) => a.id == go.assetId,
+        orElse: () => Asset(id: '', path: '', type: ''),
+      );
+      if (asset.path.isEmpty) continue;
+
+      try {
+        final node = await ModelImport.loadModel('${ProjectStore.instance.projectPath}\\${asset.path}');
+
+        node.localTransform = Matrix4.compose(
+          Vector3(
+            go.transform.position.x,
+            go.transform.position.y,
+            go.transform.position.z,
+          ),
+          Quaternion.euler(
+            radians(go.transform.rotation.x),
+            radians(go.transform.rotation.y),
+            radians(go.transform.rotation.z),
+          ),
+          Vector3(
+            go.transform.scale.x,
+            go.transform.scale.y,
+            go.transform.scale.z,
+          ),
+        );
+
+        scene.add(node);
+        _spawned.add(node);
+      } catch (e) {
+        debugPrint('Error loading model: $e');
+      }
+    }
   }
 
   @override
   void dispose() {
+    for (final n in _spawned) {
+      scene.remove(n);
+    }
     _ticker.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!ready) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
     return Focus(
       autofocus: true,
       onKeyEvent: (node, event) {
-        if (event is KeyDownEvent) {
-          controller.onKeyDown(event.logicalKey);
-        } else if (event is KeyUpEvent) {
-          controller.onKeyUp(event.logicalKey);
-        }
+        if (event is KeyDownEvent) controller.onKeyDown(event.logicalKey);
+        if (event is KeyUpEvent) controller.onKeyUp(event.logicalKey);
         return KeyEventResult.handled;
       },
       child: Listener(
